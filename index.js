c4575a1ca55a0319a3292901b4c402b9017608a3
Davidson <davidson.lucas.souza@outlook.com>
2020-12-22 12:13:01 -0300

    log and comments

--- index.js
+++ index.js
@@ -10,7 +10,19 @@ const config = require("./config.json");
 const port = config.port || 8080;
 const host = config.host || "localhost"
 
-log("System startup")
+/** Catch errors, don't break */
+process.on("uncaughtException", (e) =>
+{
+  log(e, true);
+});
+process.on("unhandledRejection", (e) =>
+{
+  log(e, true);
+});
+/**Let's log the time of startup */
+log("System startup");
+
+/**Listen to the API port */
 app.listen(port, host, () =>
 {
   log(`Listening to ${port} on ${host}`);
